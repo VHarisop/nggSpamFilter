a1f3f3d7fc3d18f06281e929cd356c99efc617d5
VHarisop <the.machine.headed@gmail.com>
2015-10-05 18:02:23 +0300

    Removed comments, added license notice

--- multiclass/Main.java
+++ multiclass/Main.java
@@ -5,22 +5,18 @@ import java.util.Arrays;
 import dataset.ConfusionMatrix;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		/* File directory = new File(args[0]);
-		File[] dirList = directory.listFiles(new FilenameFilter() {
-			@Override
-			public boolean accept(File current, String name) {
-				return new File(current, name).isDirectory();
-			}
-		});
 		
-		System.out.println(Arrays.toString(dirList)); */
-		
+		/* create a multiclass classifier on base directory 
+		 * and perform classification on all categories
+		 */
 		NggClassifier nggc = new NggClassifier(args[0]);
 		ConfusionMatrix cnf = nggc.classify_all_categories();
+
+		/* print the classification results
+		 * for every class (precision & recall)
+		 */
 		String[] labels = nggc.getLabels();
 		System.out.println(labels[0] + " " + Arrays.toString(cnf.precisionAndRecall(0)));
 		System.out.println(labels[1] + " " + Arrays.toString(cnf.precisionAndRecall(1)));
